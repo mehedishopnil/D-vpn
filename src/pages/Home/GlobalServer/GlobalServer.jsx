cd2741b2f4248506fdf5912ca03445ed21b81f89
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-12 00:51:26 +0600

    global server network

--- src/pages/Home/GlobalServer/GlobalServer.jsx
+++ src/pages/Home/GlobalServer/GlobalServer.jsx
@@ -9,9 +9,12 @@ const GlobalServer = () => {
   return (
     <div className="w-full bg-cover  bg-no-repeat pt-20" style={sectionStyle}>
       <div className="p-16 text-white text-center">
-        <p className="text-lg">Global Server Network</p>
-        <h1 className="text-4xl font-semibold mb-4">
-          Ultra-Fast Servers Around the World
+        <p className="text-[24px] font-bold text-[#701CB0]">
+          Global Server Network
+        </p>
+        <h1 className=" text-[48px] leading-[56px] font-bold mb-4">
+          Ultra-Fast Servers Around
+          <br /> the World
         </h1>
         <p className="text-lg">
           Choose among 1000+ high-speed VPN servers and enjoy fast and stable
@@ -20,27 +23,30 @@ const GlobalServer = () => {
       </div>
 
       <div className="flex flex-col gap-5 items-center justify-center">
-        <img className="w-[544px] h-[924px]" src={iphoneIcon} alt="" />
-
-        <div>
-        <div className="flex justify-between items-center gap-10 px-10 py-5 -mt-20 rounded-lg bg-[#ffffff30] shadow-lg text-black">
-          <div className="text-center">
-            <h1 className="text-2xl text-[#701CB0] font-bold">100+</h1>
-            <p className="text-[#797E86]">Countries</p>
-          </div>
-          <div className="text-center">
-            <h1 className="text-2xl text-[#701CB0] font-bold">1,000+</h1>
-            <p className="text-[#797E86]">Servers</p>
-          </div>
-          <div className="text-center">
-            <h1 className="text-2xl text-[#701CB0] font-bold">100+</h1>
-            <p className="text-[#797E86]">Locations</p>
-          </div>
-          <div className="text-center">
-            <h1 className="text-2xl text-[#701CB0] font-bold">Unlimited</h1>
-            <p className="text-[#797E86]">Bandwidth</p>
-          </div>
+        <div className="">
+          <img className="w-[544px] h-[924px]" src={iphoneIcon} alt="" />  
         </div>
+       
+
+        <div className="flex flex-col justify-center items-center -mt-[400px] w-full h-[380px] bg-[#000000]">
+          <div className="flex justify-between w-1/2 items-center gap-10 px-10 py-5 -mt-[200px] rounded-lg border border-[#ffffff10]  bg-[#ffffff10]  text-black">
+            <div className="text-center">
+              <h1 className="text-2xl text-[#701CB0] font-bold">100+</h1>
+              <p className="text-[#797E86]">Countries</p>
+            </div>
+            <div className="text-center">
+              <h1 className="text-2xl text-[#701CB0] font-bold">1,000+</h1>
+              <p className="text-[#797E86]">Servers</p>
+            </div>
+            <div className="text-center">
+              <h1 className="text-2xl text-[#701CB0] font-bold">100+</h1>
+              <p className="text-[#797E86]">Locations</p>
+            </div>
+            <div className="text-center">
+              <h1 className="text-2xl text-[#701CB0] font-bold">Unlimited</h1>
+              <p className="text-[#797E86]">Bandwidth</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
