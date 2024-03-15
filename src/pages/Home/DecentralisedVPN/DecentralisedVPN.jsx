44b4287cf18ffb9edd5924a05b614178160d4e79
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-16 01:48:19 +0600

    decentralised respnsive done

--- src/pages/Home/DecentralisedVPN/DecentralisedVPN.jsx
+++ src/pages/Home/DecentralisedVPN/DecentralisedVPN.jsx
@@ -2,16 +2,16 @@ import demoImg from "../../../assets/Image Placeholder.png";
 
 const DecentralisedVPN = () => {
   return (
-    <div className="relative overflow-hidden">
+    <div className="relative mt-24 lg:mt-0 overflow-hidden">
       <div className="container lg:flex lg:flex-col lg:items-center mx-auto px-4 lg:px-8 py-10">
         <div className="flex flex-col items-center gap-5">
-          <h1 className="text-4xl w-1/2 lg:text-5xl lg:leading-snug text-center text-white mb-12 lg:mb-16">
+          <h1 className="text-4xl lg:w-1/2 leading-snug lg:text-5xl lg:leading-snug text-center text-white mb-8">
             Key features of the decentralised VPN app
           </h1>
-
-          <div className="flex lg:flex  justify-center gap-5">
-            <div className=" lg:flex items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
-              <div>
+          {/* First row */}
+          <div className="flex flex-col lg:grid lg:grid-cols-2  justify-center gap-5 overflow-hidden">
+            <div className="flex flex-col lg:flex-row items-center lg:flex lg:items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
+              <div className=" text-center lg:text-start">
                 <h1 className="text-2xl lg:text-3xl text-white mb-2">
                   Lorem ipsum dolor
                 </h1>
@@ -20,11 +20,14 @@ const DecentralisedVPN = () => {
                   aliquam porttitor sed tellus nisl.
                 </p>
               </div>
-              <img src={demoImg} alt="image" className="w-40 lg:w-auto" />
+              <img
+                src={demoImg}
+                alt="image"
+                className="cover w-auto mt-3 lg:mt-0  lg:w-auto"
+              />
             </div>
-
-            <div className=" lg:flex items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
-              <div>
+            <div className="flex flex-col lg:flex-row items-center lg:flex lg:items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
+              <div className=" text-center lg:text-start">
                 <h1 className="text-2xl lg:text-3xl text-white mb-2">
                   Lorem ipsum dolor
                 </h1>
@@ -33,10 +36,17 @@ const DecentralisedVPN = () => {
                   aliquam porttitor sed tellus nisl.
                 </p>
               </div>
-              <img src={demoImg} alt="image" className="w-40 lg:w-auto" />
+              <img
+                src={demoImg}
+                alt="image"
+                className="cover w-auto mt-3 lg:mt-0  lg:w-auto"
+              />
             </div>
+            
           </div>
         </div>
+
+        {/* Second row */}
         <div className="grid lg:grid-cols-3 mt-5 gap-5 w-full lg:w-[1300px] border rounded-xl border-[#ffffff30] bg-[#ffffff25] p-8 ">
           <div className="col-span-2 flex flex-col justify-center">
             <h1 className="text-2xl lg:text-3xl text-white mb-2">
@@ -55,23 +65,11 @@ const DecentralisedVPN = () => {
             className="w-full lg:w-[457px] h-[324px] rounded-r-xl"
           />
         </div>
-        <div className="flex flex-col lg:flex-row justify-center mt-5">
-          <div className="flex lg:flex  justify-center gap-5">
-            <div className=" lg:flex items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
-              <div>
-                <h1 className="text-2xl lg:text-3xl text-white mb-2">
-                  Lorem ipsum dolor
-                </h1>
-                <p className="text-white text-lg lg:w-[342px] font-normal">
-                  Lorem ipsum dolor sit amet consectetur. Eget nunc ut morbi
-                  aliquam porttitor sed tellus nisl.
-                </p>
-              </div>
-              <img src={demoImg} alt="image" className="w-40 lg:w-auto" />
-            </div>
 
-            <div className=" lg:flex items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
-              <div>
+        {/* Third row */}
+        <div className="flex flex-col lg:grid lg:grid-cols-2 mt-5 justify-center gap-5 overflow-hidden">
+            <div className="flex flex-col lg:flex-row items-center lg:flex lg:items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
+              <div className=" text-center lg:text-start">
                 <h1 className="text-2xl lg:text-3xl text-white mb-2">
                   Lorem ipsum dolor
                 </h1>
@@ -80,10 +78,30 @@ const DecentralisedVPN = () => {
                   aliquam porttitor sed tellus nisl.
                 </p>
               </div>
-              <img src={demoImg} alt="image" className="w-40 lg:w-auto" />
+              <img
+                src={demoImg}
+                alt="image"
+                className="cover w-auto mt-3 lg:mt-0  lg:w-auto"
+              />
             </div>
+            <div className="flex flex-col lg:flex-row items-center lg:flex lg:items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
+              <div className=" text-center lg:text-start">
+                <h1 className="text-2xl lg:text-3xl text-white mb-2">
+                  Lorem ipsum dolor
+                </h1>
+                <p className="text-white text-lg lg:w-[342px] font-normal">
+                  Lorem ipsum dolor sit amet consectetur. Eget nunc ut morbi
+                  aliquam porttitor sed tellus nisl.
+                </p>
+              </div>
+              <img
+                src={demoImg}
+                alt="image"
+                className="cover w-auto mt-3 lg:mt-0  lg:w-auto"
+              />
+            </div>
+            
           </div>
-        </div>
       </div>
     </div>
   );
