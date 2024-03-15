aa46011499d528074f5df45f2597fec22ed4f22f
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-16 02:42:04 +0600

    responsive done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,9 +4,9 @@ import Footer from "../pages/share/Footer/Footer";
 import spinAbstract from "../assets/OBJECTS.svg";
 import rightSideBg from "../assets/rightSideBg.svg";
 import leftSideBg from "../assets/9057764-02 1.svg";
+import earthBgImage from "../assets/earthShape.svg";
 
 const Main = () => {
-  
   return (
     <div className="w-full bg-[#0F0F0F] relative">
       {/* Header */}
@@ -21,13 +21,19 @@ const Main = () => {
       {/* Circular gradient on the right side with simulated blur */}
       <div className="hidden lg:block absolute top-0 right-0 bottom-0 w-[645px] h-[745px] rounded-l-full overflow-hidden">
         {/* Circular gradient for InternetSecurity section */}
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#FAE289] rounded-l-full" style={{ transform: "translateX(90%)", filter: "blur(80px)" }}></div>
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-[#FAE289] rounded-l-full"
+          style={{ transform: "translateX(90%)", filter: "blur(80px)" }}
+        ></div>
       </div>
 
       {/* Circular gradient on the left side with simulated blur */}
       <div className="hidden z-0 lg:block absolute top-0 left-0 bottom-0 w-[645px] h-[745px] rounded-r-full overflow-hidden">
         {/* Circular gradient for D-VPN Roadmap section */}
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full" style={{ transform: "translateX(-90%)", filter: "blur(100px)" }}></div>
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full"
+          style={{ transform: "translateX(-90%)", filter: "blur(100px)" }}
+        ></div>
       </div>
 
       {/* Abstract image */}
@@ -40,13 +46,19 @@ const Main = () => {
         {/* Circular gradient on the left side Available Servers section */}
         <div className="absolute top-[13%] left-0 bottom-0 w-[700px] h-[745px] rounded-r-full overflow-hidden">
           {/* Circular gradient for Available Servers section */}
-          <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full" style={{ transform: "translateX(-90%)", filter: "blur(110px)" }}></div>
+          <div
+            className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full"
+            style={{ transform: "translateX(-90%)", filter: "blur(110px)" }}
+          ></div>
         </div>
 
         {/* Circular gradient on the right side for Available Servers section */}
         <div className="absolute top-[13%] right-0 bottom-0 lg:w-[645px] lg:h-[745px] rounded-l-full overflow-hidden">
           {/* Circular gradient for Available Servers section */}
-          <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB060] rounded-l-full" style={{ transform: "translateX(70%)", filter: "blur(90px)" }}></div>
+          <div
+            className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB060] rounded-l-full"
+            style={{ transform: "translateX(70%)", filter: "blur(90px)" }}
+          ></div>
         </div>
       </div>
 
@@ -55,7 +67,10 @@ const Main = () => {
         {/* Circular gradient on the left side */}
         <div className="absolute top-[25%] left-0 bottom-0 lg:w-[645px] lg:h-[745px] rounded-r-full overflow-hidden">
           {/* Circular gradient for MyVPN section */}
-          <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full" style={{ transform: "translateX(-90%)", filter: "blur(110px)" }}></div>
+          <div
+            className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full"
+            style={{ transform: "translateX(-90%)", filter: "blur(110px)" }}
+          ></div>
         </div>
 
         {/* Background image on the right side for MyVPN section */}
@@ -66,46 +81,69 @@ const Main = () => {
 
       {/* Circular gradient on the right side for InternetSecurity section */}
       <div className="absolute top-[30%] right-0 bottom-0 w-[445px] h-[845px] rounded-l-full overflow-hidden lg:w-auto lg:h-auto ">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB030] rounded-l-full" style={{ transform: "translateX(50%)", filter: "blur(110px)" }}></div>
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB030] rounded-l-full"
+          style={{ transform: "translateX(50%)", filter: "blur(110px)" }}
+        ></div>
       </div>
 
       {/* Background image on the left side for decentralised VPN section */}
       <div className="absolute top-1/2 lg:top-[50%] left-0 lg:left-0 transform -translate-y-1/2 rotate-180 rounded-l-full overflow-hidden ">
-        <img className="w-full h-auto lg:h-full mb-[200px]" src={leftSideBg} alt="" />
+        <img
+          className="w-full h-auto lg:h-full mb-[200px]"
+          src={leftSideBg}
+          alt=""
+        />
       </div>
 
       {/* Circular gradient on the right side for decentralised VPN section */}
       <div className="absolute top-[45%] right-0 bottom-0 w-[445px] h-[845px] rounded-l-full overflow-hidden lg:w-auto lg:h-auto ">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB030] rounded-l-full" style={{ transform: "translateX(60%)", filter: "blur(100px)" }}></div>
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB030] rounded-l-full"
+          style={{ transform: "translateX(60%)", filter: "blur(100px)" }}
+        ></div>
       </div>
 
       {/* Circular gradient on the left side for D-VPN Roadmap section */}
-<div className="absolute top-[58%] left-0 bottom-0  rounded-r-full overflow-hidden lg:w-[645px] lg:h-[745px] w-full  h-full">
-  <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full" style={{ transform: "translateX(-90%)", filter: "blur(110px)" }}></div>
-</div>
-
-
+      <div className="absolute hidden lg:block top-[58%] left-0 bottom-0  rounded-r-full overflow-hidden lg:w-[645px] lg:h-[745px] w-full  h-full">
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full"
+          style={{ transform: "translateX(-90%)", filter: "blur(110px)" }}
+        ></div>
+      </div>
 
       {/* Circular gradient on the left side for total supply section */}
-<div className="absolute top-[70%] left-0 bottom-0 lg:w-[645px] lg:h-[745px] rounded-r-full overflow-hidden w-full  h-full ">
-  <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full" style={{ transform: "translateX(-85%)", filter: "blur(110px)" }}></div>
-</div>
-
+      <div className="absolute hidden lg:block top-[70%] left-0 bottom-0 lg:w-[645px] lg:h-[745px] rounded-r-full overflow-hidden w-full  h-full ">
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090] rounded-r-full"
+          style={{ transform: "translateX(-85%)", filter: "blur(110px)" }}
+        ></div>
+      </div>
 
       {/* Circular gradient on the right side for total supply section */}
       <div className="absolute  top-[63%] right-0 bottom-0 w-[445px] h-[845px] rounded-l-full overflow-hidden lg:w-auto lg:h-auto ">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB030] rounded-l-full" style={{ transform: "translateX(50%)", filter: "blur(110px)" }}></div>
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB030] rounded-l-full"
+          style={{ transform: "translateX(50%)", filter: "blur(110px)" }}
+        ></div>
       </div>
 
       {/* Circular gradient on the left side for Global Server Section */}
-      <div className="absolute top-[88%] left-0 bottom-0 lg:w-[645px] lg:h-[745px] rounded-r-full overflow-hidden w-xl  h-xl">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090]  rounded-r-full" style={{ transform: "translateX(-90%)", filter: "blur(110px)" }}></div>
+      <div className=" absolute hidden lg:block z-10 top-[88%] left-0 bottom-0 lg:w-[645px] lg:h-[745px] rounded-r-full overflow-hidden w-xl  h-xl">
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-[#9C25EA90] to-[#701CB090]  rounded-r-full"
+          style={{ transform: "translateX(-90%)", filter: "blur(110px)" }}
+        ></div>
       </div>
 
       {/* Circular gradient on the right side for footer section */}
-      <div className="absolute  top-[90%] right-0 bottom-0 w-[445px] h-[845px] rounded-l-full overflow-hidden lg:h-auto ">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB030] rounded-l-full" style={{ transform: "translateX(50%)", filter: "blur(150px)" }}></div>
+      <div className="absolute hidden lg:block  z-10 top-[90%] right-0 bottom-0 w-[445px] h-[845px] rounded-l-full overflow-hidden lg:h-auto ">
+        <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-l from-[#9C25EA75] to-[#701CB030] rounded-l-full"
+          style={{ transform: "translateX(50%)", filter: "blur(150px)" }}
+        ></div>
       </div>
+
     </div>
   );
 };
--- src/pages/Home/GlobalServer/GlobalServer.jsx
+++ src/pages/Home/GlobalServer/GlobalServer.jsx
@@ -4,28 +4,22 @@ import { TbWorld } from "react-icons/tb";
 
 const GlobalServer = () => {
   return (
-    <div className="relative overflow-hidden">
-      {/* Background Image */}
-      <div
-        className="relative bg-cover bg-no-repeat py-16"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        {/* Main content */}
-        <div className="container mx-auto text-white text-center">
-          <p className="text-2xl font-bold text-purple-700">Global Server Network</p>
-          <h1 className="text-4xl leading-10 lg:leading-14 font-bold mb-4">
-            Ultra-Fast Servers Around the World
-          </h1>
-          <p className="text-lg lg:w-10/12 mx-auto">
-            Choose among 1000+ high-speed VPN servers and enjoy fast and stable connections anywhere.
-          </p>
-        </div>
+    <div className="relative lg:mt-[200px] overflow-hidden h-screen" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
+      {/* Main content */}
+      <div className="flex flex-col items-center gap-5 text-white text-center pt-16">
+        <p className="text-2xl font-bold text-purple-700">Global Server Network</p>
+        <h1 className="text-4xl lg:w-[500px] leading-10 lg:leading-14 font-bold mb-4">
+          Ultra-Fast Servers Around the World
+        </h1>
+        <p className="text-[24px] font-normal  lg:w-[628px] mx-auto">
+          Choose among 1000+ high-speed VPN servers and enjoy fast and stable connections anywhere.
+        </p>
       </div>
 
       {/* Server features section */}
-      <div className="relative flex flex-col lg:flex-row items-center justify-center gap-5 mt-10 lg:mt-20">
+      <div className="relative flex flex-col items-center justify-center gap-5 mt-10 lg:mt-20">
         <div className="w-full lg:w-1/2 relative">
-          <img className="w-3/4 lg:w-full lg:h-1/2 mx-auto" src={iphoneIcon} alt="iPhone Icon" />
+          <img className="w-3/4 lg:w-[544px] lg:h-[924px] mx-auto" src={iphoneIcon} alt="iPhone Icon" />
           <div className="absolute w-32 h-32 bg-[#701CB0] rounded-full top-[50%] right-[10%] lg:right-[5%] transform -translate-y-1/2 lg:translate-y-0">
             <p className="absolute text-white text-6xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"><TbWorld /></p>
           </div>
@@ -53,7 +47,6 @@ const GlobalServer = () => {
           </div>
         </div>
       </div>
-
     </div>
   );
 };
