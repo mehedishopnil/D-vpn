c939978a611f2abde0d1e062bd75ef7bbc4abce9
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-16 11:18:41 +0600

    responsive done

--- src/pages/Home/GlobalServer/GlobalServer.jsx
+++ src/pages/Home/GlobalServer/GlobalServer.jsx
@@ -5,13 +5,14 @@ import { TbWorld } from "react-icons/tb";
 const GlobalServer = () => {
   return (
     <div
-      className="flex flex-col items-center"
+      className="flex flex-col items-center mt-32"
       style={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: "cover",
+        width: "100%",
       }}
     >
-      <div className="relative lg:mt-[200px] h-screen">
+      <div className="relative  h-screen">
         {/* Main content */}
         <div className="flex flex-col items-center gap-5 text-white text-center pt-16">
           <p className="text-2xl font-bold text-purple-700">
@@ -20,7 +21,7 @@ const GlobalServer = () => {
           <h1 className="text-4xl lg:w-[500px] leading-10 lg:leading-14 font-bold mb-4">
             Ultra-Fast Servers Around the World
           </h1>
-          <p className="text-[24px] font-normal lg:w-[628px] mx-auto">
+          <p className="text-[18px] lg:text-[18px] font-normal lg:w-[628px] mx-auto">
             Choose among 1000+ high-speed VPN servers and enjoy fast and stable
             connections anywhere.
           </p>
@@ -29,12 +30,12 @@ const GlobalServer = () => {
         {/* Image (iphoneIcon) */}
         <div className="relative flex justify-center mt-10">
           <img
-            className="w-3/4 lg:w-[544px] lg:h-[924px] mx-auto"
+            className=" w-3/4 h-10/12  lg:w-[544px] lg:h-[924px] mx-auto"
             src={iphoneIcon}
             alt="iPhone Icon"
           />
-          <div className="absolute w-32 h-32 bg-[#701CB0] rounded-full top-[50%] right-[10%] lg:right-[5%] transform -translate-y-1/2 lg:translate-y-0">
-            <p className="absolute text-white text-6xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="absolute w-16 h-16 lg:w-32 lg:h-32 bg-[#701CB0] rounded-full top-[50%] lg:top-[35%] right-[10%] lg:right-[0] transform -translate-y-1/2 lg:translate-y-0">
+            <p className="absolute text-white text-4xl lg:text-6xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               <TbWorld />
             </p>
           </div>
@@ -42,8 +43,8 @@ const GlobalServer = () => {
       </div>
 
       {/* Server features grid */}
-      <div className="w-full lg:w-1/2 z-10 pb-16 flex flex-col items-center justify-center mt-10 lg:mt-20 bg-black">
-        <div className=" lg:flex lg:justify-center space-y-5 gap-10 rounded-lg border border-[#ffffff35] bg-[#ffffff25] py-5 px-10 overflow-hidden">
+      <div className="w-full lg:w-full z-10 pb-16 flex flex-col items-center justify-center mt-10 lg:mt-20 bg-black">
+        <div className=" lg:flex lg:justify-center space-y-5 lg:space-y-0 gap-10 rounded-lg border border-[#ffffff35] bg-[#ffffff25] py-5 px-10 overflow-hidden">
           <div className="text-center">
             <h1 className="text-3xl lg:text-4xl font-bold text-purple-700">
               100+
