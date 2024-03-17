cf36a5cfb2c085d34484da7306cb98c275e30d56
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-17 15:04:58 +0600

    decentralised vpn responsive done

--- src/pages/Home/DecentralisedVPN/DecentralisedVPN.jsx
+++ src/pages/Home/DecentralisedVPN/DecentralisedVPN.jsx
@@ -2,106 +2,78 @@ import demoImg from "../../../assets/Image Placeholder.png";
 
 const DecentralisedVPN = () => {
   return (
-    <div className="relative mt-24 lg:mt-0 overflow-hidden">
-      <div className="container lg:flex lg:flex-col lg:items-center mx-auto px-4 lg:px-8 py-10">
+    <div className="relative mt-24 lg:mt-0 ">
+      <div className="container mx-auto px-4 lg:px-8 py-10">
         <div className="flex flex-col items-center gap-5">
           <h1 className="text-4xl lg:w-1/2 leading-snug lg:text-5xl lg:leading-snug text-center text-white mb-8">
             Key features of the decentralised VPN app
           </h1>
+
           {/* First row */}
-          <div className="flex flex-col lg:grid lg:grid-cols-2  justify-center gap-5 overflow-hidden">
-            <div className="flex flex-col lg:flex-row items-center lg:flex lg:items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
-              <div className=" text-center lg:text-start">
-                <h1 className="text-2xl lg:text-3xl text-white mb-2">
-                  Lorem ipsum dolor
-                </h1>
-                <p className="text-white text-lg lg:w-[342px] font-normal">
-                  Lorem ipsum dolor sit amet consectetur. Eget nunc ut morbi
-                  aliquam porttitor sed tellus nisl.
-                </p>
-              </div>
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5 lg:gap-5">
+  {/* first layout */}
+  <div className="grid md:grid-cols-2 lg:grid-cols-3 bg-[#ffffff25] rounded-xl p-5 border border-[#ffffff30]">
+    <div className="text-center lg:flex lg:flex-col lg:text-start lg:justify-center md:col-span-1 lg:col-span-2">
+      <h1 className="text-xl  lg:text-2xl text-white mb-2">
+        Lorem ipsum dolor
+      </h1>
+      <p className="text-white text-[14px]  md:w-full lg:w-full font-normal">
+        Lorem ipsum dolor sit amet consectetur. Eget nunc ut morbi aliquam porttitor sed tellus nisl.
+      </p>
+    </div>
+    <div className="flex justify-center items-center mt-5">
+      <img
+        src={demoImg}
+        alt="image"
+        className="w-[400px] h-[250px] md:w-auto md:h-auto lg:w-auto lg:h-auto"
+      />
+    </div>
+  </div>
+
+  {/* Second layout */}
+  <div className="grid md:grid-cols-2 lg:grid-cols-3 bg-[#ffffff25] rounded-xl p-5 border border-[#ffffff30]">
+    <div className="text-center lg:flex lg:flex-col lg:text-start lg:justify-center md:col-span-1 lg:col-span-2">
+      <h1 className="text-xl  lg:text-2xl text-white mb-2">
+        Lorem ipsum dolor
+      </h1>
+      <p className="text-white text-[14px]  md:w-full lg:w-full font-normal">
+        Lorem ipsum dolor sit amet consectetur. Eget nunc ut morbi aliquam porttitor sed tellus nisl.
+      </p>
+    </div>
+    <div className="flex justify-center items-center mt-5">
+      <img
+        src={demoImg}
+        alt="image"
+        className=" w-[400px] h-[250px] md:w-auto md:h-auto lg:w-auto lg:h-auto"
+      />
+    </div>
+  </div>
+</div>
+
+          {/* Second row */}
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-5 bg-[#ffffff25] rounded-xl p-5 gap-5 border border-[#ffffff30]">
+            {/* Text content */}
+            <div className="text-center md:text-start lg:text-start lg:col-span-3 flex flex-col justify-center">
+              <h1 className="text-xl lg:text-2xl text-white mb-2">
+                Lorem ipsum dolor
+              </h1>
+              <p className="text-white text-[14px] md:w-full lg:w-full font-normal">
+                Lorem ipsum dolor sit amet consectetur. Ultrices neque faucibus
+                nisl et facilisi luctus. Nisl eget libero at et nascetur. In
+                aliquam donec elementum ante amet at scelerisque eu. Amet
+                pellentesque praesent euismod.
+              </p>
+            </div>
+            {/* Image */}
+            <div className=" lg:col-span-2 lg:text-right flex justify-center md:justify-end lg:justify-end">
               <img
                 src={demoImg}
                 alt="image"
-                className="cover w-auto mt-3 lg:mt-0  lg:w-auto"
+                className="w-[400px] h-[250px] mt-3 lg:mt-0"
               />
             </div>
-            <div className="flex flex-col lg:flex-row items-center lg:flex lg:items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
-              <div className=" text-center lg:text-start">
-                <h1 className="text-2xl lg:text-3xl text-white mb-2">
-                  Lorem ipsum dolor
-                </h1>
-                <p className="text-white text-lg lg:w-[342px] font-normal">
-                  Lorem ipsum dolor sit amet consectetur. Eget nunc ut morbi
-                  aliquam porttitor sed tellus nisl.
-                </p>
-              </div>
-              <img
-                src={demoImg}
-                alt="image"
-                className="cover w-auto mt-3 lg:mt-0  lg:w-auto"
-              />
-            </div>
-            
           </div>
         </div>
-
-        {/* Second row */}
-        <div className="grid lg:grid-cols-3 mt-5 gap-5 w-full lg:w-[1300px] border rounded-xl border-[#ffffff30] bg-[#ffffff25] p-8 ">
-          <div className="col-span-2 flex flex-col justify-center">
-            <h1 className="text-2xl lg:text-3xl text-white mb-2">
-              Lorem ipsum dolor
-            </h1>
-            <p className="text-white text-lg font-normal">
-              Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur. Ultrices
-              neque faucibus nisl et facilisi luctus. Nisl eget libero at et
-              nascetur. In aliquam donec elementum ante amet at scelerisque eu.
-              Amet pellentesque praesent euismod.
-            </p>
-          </div>
-          <img
-            src={demoImg}
-            alt="image"
-            className="w-full lg:w-[457px] h-[324px] rounded-r-xl"
-          />
-        </div>
-
-        {/* Third row */}
-        <div className="flex flex-col lg:grid lg:grid-cols-2 mt-5 justify-center gap-5 overflow-hidden">
-            <div className="flex flex-col lg:flex-row items-center lg:flex lg:items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
-              <div className=" text-center lg:text-start">
-                <h1 className="text-2xl lg:text-3xl text-white mb-2">
-                  Lorem ipsum dolor
-                </h1>
-                <p className="text-white text-lg lg:w-[342px] font-normal">
-                  Lorem ipsum dolor sit amet consectetur. Eget nunc ut morbi
-                  aliquam porttitor sed tellus nisl.
-                </p>
-              </div>
-              <img
-                src={demoImg}
-                alt="image"
-                className="cover w-auto mt-3 lg:mt-0  lg:w-auto"
-              />
-            </div>
-            <div className="flex flex-col lg:flex-row items-center lg:flex lg:items-center gap-5 border rounded-xl p-5 border-[#ffffff30] bg-[#ffffff25] ">
-              <div className=" text-center lg:text-start">
-                <h1 className="text-2xl lg:text-3xl text-white mb-2">
-                  Lorem ipsum dolor
-                </h1>
-                <p className="text-white text-lg lg:w-[342px] font-normal">
-                  Lorem ipsum dolor sit amet consectetur. Eget nunc ut morbi
-                  aliquam porttitor sed tellus nisl.
-                </p>
-              </div>
-              <img
-                src={demoImg}
-                alt="image"
-                className="cover w-auto mt-3 lg:mt-0  lg:w-auto"
-              />
-            </div>
-            
-          </div>
       </div>
     </div>
   );
